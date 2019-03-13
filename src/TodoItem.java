d7cb8505771e2d9d56f2537bccc6cf4ca51cda5d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-13 18:15:48 +0100

    Add TodoItem logic

--- src/TodoItem.java
+++ src/TodoItem.java
@@ -1,2 +1,34 @@
+import java.time.LocalDate;
+
 public class TodoItem {
+
+    private String title;
+    private LocalDate deadline;
+    private boolean isDone = false;
+
+    public TodoItem(String title, LocalDate deadline) {
+        this.title = title;
+        this.deadline = deadline;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public LocalDate getDeadline() {
+        return this.deadline;
+    }
+
+    public boolean mark() {
+        return this.isDone = true;
+    }
+
+    public boolean unmark() {
+        return this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.deadline + " " + this.title;
+    }
 }
