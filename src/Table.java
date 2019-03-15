2ae384c7b818680e8f225538503d74752b3bba99
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-15 19:54:31 +0100

    Refactor function get columns width

--- src/Table.java
+++ src/Table.java
@@ -12,46 +12,79 @@ public class Table {
     public Integer[] getColumnsWidth() {
         Integer[] columnsWidth = {0, 0};
         for (Map.Entry<String, TodoQuarter> entry : todoMatrix.getQuarters().entrySet()) {
-            if (entry.getKey().equals("IU") || entry.getKey().equals("NU")) {
-                for (int i = 0; i < entry.getValue().getItems().size(); i++) {
-                    TodoItem todoItemAtIndex = entry.getValue().getItem(i);
-                    if (columnsWidth[1] < todoItemAtIndex.getDeadline().toString().length()
-                            + todoItemAtIndex.getTitle().length()) {
-                        columnsWidth[1] = todoItemAtIndex.getDeadline().toString().length()
-                                + todoItemAtIndex.getTitle().length();
-                    }
-                }
-            } else if (entry.getKey().equals("IN") || entry.getKey().equals("NN")) {
-                for (int i = 0; i < entry.getValue().getItems().size(); i++) {
-                    TodoItem todoItemAtIndex = entry.getValue().getItem(i);
-                    if (columnsWidth[0] < todoItemAtIndex.getDeadline().toString().length()
-                            + todoItemAtIndex.getTitle().length()) {
-                        columnsWidth[0] = todoItemAtIndex.getDeadline().toString().length()
-                                + todoItemAtIndex.getTitle().length();
-                    }
-                }
+            String entryKey = entry.getKey();
+            TodoQuarter entryValue = entry.getValue();
+
+            if (entryKey.equals("IU") || entryKey.equals("NU")) {
+                columnsWidth[1] = firstColumnWidth(entryValue, columnsWidth[1]);
+            } else if (entryKey.equals("IN") || entryKey.equals("NN")) {
+                columnsWidth[0] = secondColumnWidth(entryValue, columnsWidth[0]);
             }
         }
         return columnsWidth;
     }
 
+    private int firstColumnWidth(TodoQuarter entryValue, int firstColumnLength) {
+        for (int i = 0; i < entryValue.getItems().size(); i++) {
+            int deadlineLength = entryValue.getItem(i).getDeadline().toString().length();
+            int titleLength = entryValue.getItem(i).getTitle().length();
+
+            if (firstColumnLength < deadlineLength + titleLength) {
+                firstColumnLength = deadlineLength + titleLength;
+            }
+        }
+        return  firstColumnLength;
+    }
+
+    private int secondColumnWidth(TodoQuarter entryValue, int secondColumnLength) {
+        for (int i = 0; i < entryValue.getItems().size(); i++) {
+            int deadlineLength = entryValue.getItem(i).getDeadline().toString().length();
+            int titleLength = entryValue.getItem(i).getTitle().length();
+
+            if (secondColumnLength < deadlineLength + titleLength) {
+                secondColumnLength = deadlineLength + titleLength;
+            }
+        }
+        return secondColumnLength;
+    }
+
+
     public void createTable(TodoMatrix todoMatrix) {
         String format = "";
+        String reverseformat = "";
         int extraSpaces = 10;
 
-        for (int column : getColumnsWidth()) {
-            format += "%-" + (column + extraSpaces) + "s ";
+        for (int i = 0; i < getColumnsWidth().length; i++) {
+            format += "%-" + (getColumnsWidth()[i] + extraSpaces) + "s ";
         }
         format += "%n";
 
-        TodoQuarter IU =todoMatrix.getQuarter("IU");
-        TodoQuarter NU= todoMatrix.getQuarter("NU");
+        for (int i = getColumnsWidth().length; i > 0; i--) {
+            reverseformat += "%-" + (getColumnsWidth()[i - 1] + extraSpaces) + "s ";
+        }
+        reverseformat += "%n";
+
+        TodoQuarter IU = todoMatrix.getQuarter("IU");
+        TodoQuarter NU = todoMatrix.getQuarter("NU");
 
         TodoQuarter IN = todoMatrix.getQuarter("IN");
         TodoQuarter NN = todoMatrix.getQuarter("NN");
         System.out.println(format);
-        for (int i = 0; i < NN.getItems().size(); i++) {
+
+
+        int first = Math.abs(IN.getItems().size() - NN.getItems().size());
+        for (int i = 0; i < Math.min(NN.getItems().size(), IN.getItems().size()); i++) {
             System.out.printf(format, IN.getItem(i), NN.getItem(i));
+        }
+
+        if (IN.getItems().size() > NN.getItems().size()) {
+            for (int j = 1; j <= first; j++) {
+                System.out.printf(format, IN.getItem(IN.getItems().size() - j), "");
+            }
+        } else {
+            for (int j = 1; j <= first; j++) {
+                System.out.printf(reverseformat, "", NN.getItem(NN.getItems().size() - j), "");
+            }
         }
 
 //        System.out.println(format);
@@ -59,7 +92,6 @@ public class Table {
 //        System.out.println();
 //        System.out.printf(format, IU, NU);
 //        System.out.printf(format, IN, NN);
+
     }
-
-
 }
